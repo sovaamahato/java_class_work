bc41051055a059828448757e7dea583d89a3cb9a
sova <mahatoshobha143@gmail.com>
2023-03-12 11:36:40 +0545

    interface in java

---
+++ interface/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Rectangle rect = new Rectangle();
+        rect.printArea();
+
+    }
+}
---
+++ interface/src/Rectangle.java
@@ -0,0 +1,12 @@
+public class Rectangle implements Shape, AnotherShape{
+    @Override
+    public void printArea(){
+        System.out.println("from shape");
+    }
+    ,
+
+    @Override
+    public void getName() {
+        System.out.println(" from another shape");
+    }
+}
