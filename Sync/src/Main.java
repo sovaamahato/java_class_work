dc66d0811ad51a36b13aca4a200ab4131b70fd81
sova <mahatoshobha143@gmail.com>
2023-03-16 11:28:43 +0545

    multiThreading in java

---
+++ Sync/src/Main.java
@@ -0,0 +1,44 @@
+public class Main {
+    public static void main(String[] args) {
+        Process1 p1 =new Process1();
+        Process2 p2 =new Process2();
+
+
+        p1.start();
+        System.out.println("----------------------------------------------");
+        p2.start();
+//for(int i=0;i<10;i++)
+//{
+//    p1.call();
+//}
+
+
+
+//        for(int i=0;i<10;i++)
+//        {
+//            p2.call();
+//        }
+    }
+}
+
+class Process1 extends  Thread{
+    @Override
+    public void run() {
+        for(int i=0;i<1000;i++)
+        {
+            System.out.println("process 1 calling...\n*************************************");
+        }
+
+    }
+
+}
+
+class Process2 extends  Thread {
+    @Override
+    public void run() {
+        for (int i = 0; i < 1000; i++) {
+            System.out.println("process 2 calling..\n----------------------------------------------.");
+        }
+    }
+
+}
